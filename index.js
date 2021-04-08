060e1e9f5fcd4e5158553340290f0a2efe8903cc
DavidRMorphew <morphewd@umich.edu>
2021-04-08 11:33:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+const mapToNegativize = (sourceArray) => {
+    const newArray = []
+    sourceArray.forEach((element) => {newArray.push(element * -1)})
+    return newArray
+};
+
+function mapToNoChange(sourceArray){
+    const newArray = [...sourceArray]
+    return newArray
+}
+
+const mapToDouble = (sourceArray) => {
+    const newArray = []
+    sourceArray.forEach((element) => {newArray.push(element * 2)})
+    return newArray
+};
+
+const mapToSquare = (sourceArray) => {
+    const newArray = []
+    sourceArray.forEach((element) => {newArray.push(element ** 2)})
+    return newArray
+};
